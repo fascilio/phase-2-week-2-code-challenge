398b3dd8f0db48732a48a171a6c283353471631d
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-04-29 15:45:43 +0300

    components changes

--- bot-battlr/src/components/BotCollection.js
+++ bot-battlr/src/components/BotCollection.js
@@ -1,37 +1,3 @@
-// import React from "react";
-// import { useState, useEffect } from 'react';
-
-// function BotCollection({ addToArmy }) {
-//   const [bots, setBots] = useState([]);
-
-//   useEffect(() => {
-//     fetch('http://localhost:8001/bots')
-//       .then(res => res.json())
-//       .then(data => setBots(data))
-//       .catch(error => console.log(error));
-//   }, []);
-
-//   const handleAddToArmy = (bot) => {
-//     addToArmy(bot);
-//   };
-
-//   return (
-//     <div className="bot-collection">
-//       {bots.map(bot => (
-//         <div key={bot.id} className="bot-card">
-//           <img src={bot.avatar_url} alt={bot.name} />
-//           <h2>{bot.name}</h2>
-//           <p>Health: {bot.health}</p>
-//           <p>Damage: {bot.damage}</p>
-//           <p>Armor: {bot.armor}</p>
-//           <button onClick={() => handleAddToArmy(bot)}>Add to army</button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default BotCollection;
 import React from "react";
 import { useState, useEffect } from 'react';
 
--- bot-battlr/src/components/YourBotArmy.js
+++ bot-battlr/src/components/YourBotArmy.js
@@ -1,35 +1,3 @@
-// import React from "react";
-
-// function YourBotArmy({ army, releaseBot, dischargeBot }) {
-//   const handleReleaseBot = (bot) => {
-//     releaseBot(bot);
-//   };
-
-//   const handleDischargeBot = (bot) => {
-//     dischargeBot(bot);
-//   };
-
-//   return (
-//     <div className="your-bot-army">
-//       <h2>Your Bot Army</h2>
-//       {army.map(bot => (
-//         <div key={bot.id} className="bot-card">
-//           <img src={bot.avatar_url} alt={bot.name} />
-//           <h2>{bot.name}</h2>
-//           <p>Health: {bot.health}</p>
-//           <p>Damage: {bot.damage}</p>
-//           <p>Armor: {bot.armor}</p>
-//           <button onClick={() => handleReleaseBot(bot)}>Release</button>
-//           <button className="discharge" onClick={() => handleDischargeBot(bot)}>X</button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default YourBotArmy;
-
-
 import React from "react";
 
 function YourBotArmy({ releaseBot, army, dischargeBot }) {
